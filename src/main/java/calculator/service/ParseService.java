0c16d27c60ab62ceca535f0fcb5a3c197d576194
circlezero <wy129@naver.com>
2025-10-15 23:50:19 +0900

    refactor(service): ParseService 문자열 정수 변환 시 커스텀 함수 사용

--- src/main/java/calculator/service/ParseService.java
+++ src/main/java/calculator/service/ParseService.java
@@ -27,10 +27,25 @@ public class ParseService {
         String[] tokenStrings = formula.split(delimiter.getDelimiter());
         List<Integer> parsedTokens = Arrays.stream(tokenStrings)
                 .map(String::trim)
-                .mapToInt(Integer::parseInt)
+                .mapToInt(this::stoi)
                 .boxed()
                 .toList();
         return new Tokens(parsedTokens);
     }
 
+    private Integer stoi(String token) {
+        if (token.isEmpty()) {
+            return 0;
+        }
+
+        if (!token.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("토큰에 숫자가 아닌 문자가 포함되어 있습니다");
+        }
+
+        try {
+            return Integer.parseInt(token);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("토큰이 정수 범위를 벗어났습니다");
+        }
+    }
 }
