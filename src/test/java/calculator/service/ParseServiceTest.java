038b48569ffbfa3f290a67e60e1df3d04b5b4dbb
circlezero <wy129@naver.com>
2025-10-15 22:30:09 +0900

    feat(test): ParseServiceTest 테스트 케이스 생성

---
+++ src/test/java/calculator/service/ParseServiceTest.java
@@ -0,0 +1,83 @@
+package calculator.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import calculator.model.Delimiter;
+import calculator.model.Tokens;
+import org.junit.jupiter.api.Test;
+
+public class ParseServiceTest {
+
+    @Test
+    void 커스텀_구분자_없을때_파싱() {
+        ParseService parseService = new ParseService();
+        String input = "1,2,3";
+        ParseHeaderResult parsedDelimiter = parseService.parseDelimiter(input);
+        String formula = parsedDelimiter.fomula();
+        String delimiter = parsedDelimiter.delimiter();
+
+        assertEquals("1,2,3", formula);
+        assertNull(delimiter);
+    }
+
+    @Test
+    void 커스텀_구분자_있을때_파싱() {
+        ParseService parseService = new ParseService();
+        String input = "//;\\n1;2;3";
+        ParseHeaderResult parsedDelimiter = parseService.parseDelimiter(input);
+        String formula = parsedDelimiter.fomula();
+        String delimiter = parsedDelimiter.delimiter();
+        assertEquals("1;2;3", formula);
+        assertEquals(";", delimiter);
+    }
+
+    @Test
+    void 커스텀_구분자_여러글자_있을때_파싱() {
+        ParseService parseService = new ParseService();
+        String input = "//***\\n1***2***3";
+        ParseHeaderResult parsedDelimiter = parseService.parseDelimiter(input);
+        String formula = parsedDelimiter.fomula();
+        String delimiter = parsedDelimiter.delimiter();
+        assertEquals("1***2***3", formula);
+        assertEquals("***", delimiter);
+    }
+
+    @Test
+    void 공백을_포함한_구분자_파싱_테스트() {
+        ParseService parseService = new ParseService();
+        String input = "// ; ;\\n 1; 2; 3 ";
+        ParseHeaderResult parsedResult = parseService.parseDelimiter(input);
+        String formula = parsedResult.fomula();
+        String delimiter = parsedResult.delimiter();
+        assertEquals("1; 2; 3", formula);
+        assertEquals("; ;", delimiter);
+    }
+
+    @Test
+    void 기본_구분자로_수식_파싱_테스트() {
+        ParseService parseService = new ParseService();
+        String formula = "1,2:3";
+        Delimiter delimiter = new Delimiter();
+        Tokens tokens = parseService.parseTokens(formula, delimiter);
+
+        assertEquals(3, tokens.getTokens().size());
+        assertEquals(1, tokens.getTokens().get(0));
+        assertEquals(2, tokens.getTokens().get(1));
+        assertEquals(3, tokens.getTokens().get(2));
+    }
+
+    @Test
+    void 커스텀_구분자로_수식_파싱_테스트() {
+        ParseService parseService = new ParseService();
+        String formula = "1.2:3;4";
+        Delimiter delimiter = new Delimiter(";");
+        Tokens tokens = parseService.parseTokens(formula, delimiter);
+        assertEquals(4, tokens.getTokens().size());
+        assertEquals(1, tokens.getTokens().get(0));
+        assertEquals(2, tokens.getTokens().get(1));
+        assertEquals(3, tokens.getTokens().get(2));
+        assertEquals(4, tokens.getTokens().get(3));
+    }
+
+}
