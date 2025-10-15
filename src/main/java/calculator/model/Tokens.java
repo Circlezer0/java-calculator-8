ed4075fd9f275ac80a3c6cfc4db8b974ab5ba41a
circlezero <wy129@naver.com>
2025-10-15 21:33:14 +0900

    feat(model): Tokens 객체 구현

---
+++ src/main/java/calculator/model/Tokens.java
@@ -0,0 +1,23 @@
+package calculator.model;
+
+import java.util.List;
+
+public class Tokens {
+
+    private final List<Integer> tokens;
+
+    public Tokens(List<Integer> tokens) {
+        if (tokens.stream().anyMatch(t -> t < 0)) {
+            throw new IllegalArgumentException("음수는 허용되지 않습니다.");
+        }
+        this.tokens = List.copyOf(tokens);
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (int token : tokens) {
+            sum = Math.addExact(sum, token);
+        }
+        return sum;
+    }
+}
