f11f6f8bd15bf13a0d500e9a0e7aefa0ceef855e
circlezero <wy129@naver.com>
2025-10-16 00:10:54 +0900

    feat(test): CalculatorServiceTest 테스트 케이스 생성

---
+++ src/test/java/calculator/service/CalculatorServiceTest.java
@@ -0,0 +1,26 @@
+package calculator.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import calculator.model.Tokens;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorServiceTest {
+
+    @Test
+    void test() {
+        Tokens tokens = new Tokens(List.of(1,2,3));
+        CalculatorService calculatorService = new CalculatorService();
+        int result = calculatorService.calculate(tokens);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void 오버플로우_발생_시_IllegalArgumentException() {
+        Tokens tokens = new Tokens(List.of(Integer.MAX_VALUE, 1));
+        CalculatorService calculatorService = new CalculatorService();
+        assertThrows(IllegalArgumentException.class, () -> calculatorService.calculateSum(tokens));
+    }
+}
