0bb6b4e5d42cab868d763ed75b819a7a6be04926
circlezero <wy129@naver.com>
2025-10-19 16:55:59 +0900

    docs(controller): 컨트롤러 객체에 주석 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -7,11 +7,15 @@ import calculator.service.CalculatorService;
 import calculator.service.ParseService;
 import calculator.view.CalculatorView;
 
+/**
+ * CalculatorController는 View와 Service를 활용하여
+ * 덧셈 계산기 애플리케이션의 흐름을 제어하는 클래스이다.
+ */
 public class CalculatorController {
 
-    private final CalculatorView calculatorView;
-    private final CalculatorService calculatorService;
-    private final ParseService parseService;
+    private final CalculatorView calculatorView;        // 사용자 입출력 담당
+    private final CalculatorService calculatorService;  // 덧셈 계산 로직 담당
+    private final ParseService parseService;            // 구분자, 수식 파싱 담당
 
     public CalculatorController() {
         this.calculatorView = new CalculatorView();
@@ -19,23 +23,30 @@ public class CalculatorController {
         this.parseService = new ParseService();
     }
 
+    /**
+     * 덧셈 계산기 애플리케이션의 주요 흐름을 실행하는 메서드이다.
+     */
     public void run() {
         // 1. 사용자 입력 받기
         String input = calculatorView.getInput();
 
-        // 2. 구분자 및 수식 파싱
+        // 2. 입력으로 받은 수식을 (커스텀 구분자, 수식) 두 부분으로 파싱
         ParseHeaderResult parseHeaderResult = parseService.parseDelimiter(input);
         String formula = parseHeaderResult.formula();
         String delimiterString = parseHeaderResult.delimiter();
 
         // 3. 구분자 객체 생성
         Delimiter delimiter = (delimiterString == null) ?
-                new Delimiter() :
-                new Delimiter(delimiterString);
+                new Delimiter() :                   // 커스텀 구분자가 없는 경우
+                new Delimiter(delimiterString);     // 커스텀 구분자가 있는 경우
 
+        // 4. 구분자 객체를 활용하여 나머지 수식에서 숫자들을 파싱
         Tokens tokens = parseService.parseTokens(formula, delimiter);
 
+        // 5. 파싱된 숫자들에 대해 덧셈 계산 수행
         int result = calculatorService.calculateSum(tokens);
+
+        // 6. 계산 결과 출력
         calculatorView.displayOutput(result);
     }
 }
